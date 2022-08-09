6afa91277915ed952dae08798d42b13b52831842
Lee-moran <leemoran06@hotmail.com>
2022-08-09 14:36:47 +0000

    stateful class components Updating the State, part 3

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,16 @@
 import css from './App.module.css';
 import Sidebar from './components/Sidebar';
+import StatefulGreeting from './components/StatefulGreeting';
 
 function App() {
   return (
     <div className={css.Sidebar}>
       <Sidebar/>
+      <StatefulGreeting/>
     </div>
   );
 }
+
+
 
 export default App;
