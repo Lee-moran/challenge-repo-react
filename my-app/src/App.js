bb2ae56f0fd093a802325aa3cdd5818a92e926a4
Lee-moran <leemoran06@hotmail.com>
2022-08-10 10:30:42 +0000

    challenge done, navbar element

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
 import css from './App.module.css';
 import Sidebar from './components/Sidebar';
 import StatefulGreeting from './components/StatefulGreeting';
+import NavBarSimple from './components/NavBarSimple';
 
 function App() {
   return (
     <div className={css.Sidebar}>
       <Sidebar/>
       <StatefulGreeting/>
+      <NavBarSimple/>
     </div>
   );
 }
