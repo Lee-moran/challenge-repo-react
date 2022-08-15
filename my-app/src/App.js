d5a45d3baa7aeb29c275a0537477f5cef9b0f485
Lee-moran <leemoran06@hotmail.com>
2022-08-15 11:55:31 +0000

    challenge 3 done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,14 +3,16 @@ import Sidebar from './components/Sidebar';
 import StatefulGreeting from './components/StatefulGreeting';
 import NavBarSimple from './components/NavBarSimple';
 import NavBarForm from './components/NavBarForm';
+import Content from './components/Content';
 
 function App() {
   return (
     <div className={css.Sidebar}>
-      <Sidebar/>
       <StatefulGreeting/>
       <NavBarSimple/>
       <NavBarForm/>
+      <Sidebar/>
+      <Content/>
     </div>
   );
 }
