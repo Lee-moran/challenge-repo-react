6e920510e622678dda7a7d343130343e56112bc9
Lee-moran <leemoran06@hotmail.com>
2022-08-16 13:24:04 +0000

    challenge component lifcycle method

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,15 +4,18 @@ import StatefulGreeting from './components/StatefulGreeting';
 import NavBarSimple from './components/NavBarSimple';
 import NavBarForm from './components/NavBarForm';
 import Content from './components/Content';
+import Loader from './components/Loader';
 
 function App() {
   return (
     <div className={css.Sidebar}>
+      <Loader/>
       <StatefulGreeting/>
       <NavBarSimple/>
       <NavBarForm/>
       <Sidebar/>
       <Content/>
+      
     </div>
   );
 }
---
+++ my-app/src/components/Loader.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import css from "./css/Loader.module.css";
+
+export class Loader extends Component {
+  render() {
+    return (
+      <div className= {css.Loader}></div>
+    )
+  }
+}
+
+export default Loader
+
