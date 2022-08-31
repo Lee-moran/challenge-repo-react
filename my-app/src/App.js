a7a947ebd04b77ca40e922de03e0d103437386a0
Lee-moran <leemoran06@hotmail.com>
2022-08-31 14:01:52 +0000

    Network Requests Challenge

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,28 @@
 import css from './App.module.css';
-import Sidebar from './components/Sidebar';
-import StatefulGreeting from './components/StatefulGreeting';
-import NavBarSimple from './components/NavBarSimple';
-import NavBarForm from './components/NavBarForm';
-// import Content from './components/Content';
-import Loader from './components/Loader';
-import ContentHooks from './components/ContentHooks';
+// import ContentAPI from './components/ContentAPI';
+import ContentAPIHooks from './components/ContentAPIHooks';
+// import Sidebar from './components/Sidebar';
+// import StatefulGreeting from './components/StatefulGreeting';
+// import NavBarSimple from './components/NavBarSimple';
+// import NavBarForm from './components/NavBarForm';
+// // import Content from './components/Content';
+// import Loader from './components/Loader';
+// import ContentHooks from './components/ContentHooks';
 
 function App() {
   return (
     <div className={css.Sidebar}>
-      <Loader/>
+      {/* <Loader/>
       <StatefulGreeting/>
       <NavBarSimple/>
       <NavBarForm/>
       <Sidebar/>
-      {/* <Content/> */}
+      <Content/> */}
 
-      <ContentHooks/>
+      {/* <ContentHooks/> */}
+
+      {/* <ContentAPI/> */}
+      <ContentAPIHooks/>
       
     </div>
   );
